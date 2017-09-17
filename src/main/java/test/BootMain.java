3030bf91f3d87517eca15af52ed4739a5f060f18
SantaOracle <524372977@qq.com>
2017-09-17 14:49:58 +0800

    create project

---
+++ src/main/java/test/BootMain.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@SpringBootApplication
+@MapperScan("com.yonyou.train.mapper")
+@ApiIgnore
+@EnableScheduling 
+public class BootMain extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMain.class, args);
+	}
+	
+	@RequestMapping("/help")
+	public String help() {
+		return "redirect:swagger-ui.html";
+	}
+
+}
+
